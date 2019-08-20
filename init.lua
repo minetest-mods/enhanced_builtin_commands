2034ebc4ba3eae84bf80fc1a70d44b754757c127
Panquesito7 <51391473+Panquesito7@users.noreply.github.com>
2019-08-20 17:29:26 -0500

    Translate "/killme"

--- init.lua
+++ init.lua
@@ -889,3 +889,31 @@ if core.get_modpath("sethome") then
 		end,
 	})
 end
+
+if core.get_modpath("game_commands") then
+	core.override_chatcommand("killme", {
+		description = S("Kill yourself to respawn"),
+		func = function(name)
+			local player = core.get_player_by_name(name)
+			if player then
+				if core.settings:get_bool("enable_damage") then
+					player:set_hp(0)
+					return true
+				else
+					for _, callback in pairs(core.registered_on_respawnplayers) do
+						if callback(player) then
+							return true
+						end
+					end
+
+					-- There doesn't seem to be a way to get a default spawn pos
+					-- from the lua API
+					return false, S("No static_spawnpoint defined")
+				end
+			else
+				-- Show error message if used when not logged in, eg: from IRC mod
+				return false, S("You need to be online to be killed!")
+			end
+		end
+	})
+end
